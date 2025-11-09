34e96fbe148da3476f4e85b7b56d605730bcf217
simeon <simeonlub@gmail.com>
2025-11-09 17:18:00 +0300

    Automated dotfiles backup [2025-11-09 17:18:00]

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x RESTIC_REPOSITORY "/mnt/mega/backups"
     set -x RESTIC_PASSWORD_FILE "/home/user/.config/restic/password"
+    # Добавляем локальную папку с бинарниками в PATH, если ее там еще нет
+    if not contains /home/user/.local/bin $fish_user_paths
+        fish_add_path /home/user/.local/bin
+    end
     export "MICRO_TRUECOLOR=1"
 end
